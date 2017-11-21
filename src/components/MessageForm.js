3255bef0a2289616450d1178533f3d569d8a352a
Katerina Bezlepkina <ekaterinabez@gmail.com>
2017-11-21 10:41:23 +0800

    fixed message submiting bugs

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -8,6 +8,7 @@ export default class MessageForm extends React.Component {
     super(props);
     this.state = {
       input: props.input,
+      error: '',
     }
   }
 
@@ -18,10 +19,13 @@ export default class MessageForm extends React.Component {
 
   onSubmit = (event) => {
     event.preventDefault();
-    let userInput = this.state.input;
-    let songType = keyWordsFilter(userInput); 
 
-    if (songType && userInput !== undefined) {
+    if (!this.state.input.trim().length && (event.keyCode === 13 || event.type === 'submit')) {
+      this.setState(() => ({ error: 'Please provide a message for bot' }));
+    } else if (this.state.input.trim().length && (event.keyCode === 13 || event.type === 'submit')) {
+      this.setState(() => ({ error: '' }));
+      let userInput = this.state.input;
+      let songType = keyWordsFilter(userInput); 
       this.props.onSubmit(songType, userInput);
       this.setState (() => ({input: ''}));
     }
@@ -30,16 +34,22 @@ export default class MessageForm extends React.Component {
   render() {
     return (
       <form className="message-form" onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form__error">{this.state.error}</p>}
         <div className="flexContainer">
           <textarea
             className="msgTextarea"
             type="text"
             value={this.state.input}
             onChange={this.onTextAreaChange}
+            onKeyUp={this.onSubmit}
             placeholder="ask for a song.(ex: send me a sad song)"
           >
           </textarea>
-          <button className="msgButton"></button>
+          <button 
+            className="msgButton"
+            type="submit"
+            >
+          </button>
         </div>
       </form>
     )
